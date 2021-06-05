299babbaeae6f3ec995d3d58c9bfc87c535e3838
BrandedNomad <info@brandednomad.com>
2021-06-05 19:24:21 +0700

    feat: add custom Error classes to normalize error responses

--- auth/src/controller/v0/auth/routes/sign-up.router.ts
+++ auth/src/controller/v0/auth/routes/sign-up.router.ts
@@ -5,7 +5,9 @@
 //imports
 //@ts-ignore
 import express,{Request,Response} from 'express'
-import {body, validationResult} from 'express-validator'
+import {body, validationResult} from 'express-validator';
+import {RequestValidationError} from "../../../../errors/request-validation.error";
+import {DatabaseConnectionError} from "../../../../errors/database-connection.error";
 
 //Creating a router
 //@ts-ignore
@@ -24,14 +26,14 @@ signupRouter.post('/signup',[
 
     const errors = validationResult(req);
     if(!errors.isEmpty()){
-        throw new Error("Invalid email or password");
+        throw new RequestValidationError(errors.array());
     }
 
     const {email, password} = req.body;
 
 
     console.log('Creating a user...')
-    throw new Error("Failed to connect to database")
+    throw new DatabaseConnectionError();
 
     res.send("success!")
 })
